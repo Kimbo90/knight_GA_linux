de7e18fab398671a8fd047da94a1ad88fa9a63cb
Kimbo90 <50793716+Kimbo90@users.noreply.github.com>
2026-05-01 12:32:16 +0100

    Update run_GA.R

--- run_GA.R
+++ run_GA.R
@@ -15,15 +15,12 @@ library(wateRmelon)
 
 # ---- DOWNLOAD beta matrix from OneDrive ----
 
-
 download.file(
   "https://cf-my.sharepoint.com/:u:/r/personal/joneskm10_cardiff_ac_uk/Documents/Documents/GA_transfer/beta_W6.rds?download=1",
   destfile = "beta_W6.rds",
   mode = "wb",
   method = "libcurl"
 )
-
-
 
 beta_W6 <- readRDS("beta_W6.rds")
 
